88b8f65838f55d9055c2732bef361843b9f952ab
Rick Li <li.rick.1996@gmail.com>
2019-11-24 18:43:19 -0800

    Minor change - include interest in topic list

--- MentorMe1.1/frontend/src/components/Topics.jsx
+++ MentorMe1.1/frontend/src/components/Topics.jsx
@@ -1,46 +1,54 @@
 import React, { Component } from "react";
-import axios from 'axios';
-import Card from './Card';
-import {Container} from 'react-bootstrap'
-import TopicsSearch from './TopicsSearch'
+import axios from "axios";
+import Card from "./Card";
+import { Container } from "react-bootstrap";
+import TopicsSearch from "./TopicsSearch";
 import TopicChat from "./TopicChat";
 
 class Topics extends Component {
-    state ={
-        data:[],
-        loaded:false,
-        activeID:null
-    }
-    
+  state = {
+    data: [],
+    loaded: false,
+    activeID: null
+  };
 
+  handleSetTopic = topicid => {
+    this.setState({ activeID: topicid });
+  };
 
-    handleSetTopic= (topicid)=>{
-        this.setState({activeID:topicid});
-    }
+  componentDidMount() {
+    axios
+      .get("/api/topics", {
+        headers: { "x-auth-token": sessionStorage.getItem("authToken") }
+      })
+      .then(res => {
+        console.log(res.data);
+        this.setState({ data: res.data, loaded: true });
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  }
 
-    componentDidMount() {
-        axios.get("/api/topics", {
-        }).then(res => {
-            console.log(res.data);
-            this.setState({data:res.data, loaded:true});
-        }).catch(err => {
-            console.log(err.message);
-        })
-    }
+  render() {
+    const { data, loaded, activeID } = this.state;
+    const { socket } = this.props;
+    return (
+      <React.Fragment>
+        {activeID ? (
+          <TopicChat socket={socket} topicid={activeID} />
+        ) : (
+          <TopicsSearch
+            data={data}
+            loaded={loaded}
+            onSetTopic={id => this.handleSetTopic(id)}
+          ></TopicsSearch>
+        )}
 
-    render(){
-        const {data, loaded, activeID} = this.state;
-        const { socket } = this.props;
-        return (
-        <React.Fragment>
-            {activeID?<TopicChat socket={socket} topicid={activeID}/> 
-            :<TopicsSearch data={data} loaded={loaded} onSetTopic={id=>this.handleSetTopic(id)}></TopicsSearch>}
-            
-            {/* <TopicChat socket={socket} topicid={topicid} /> */}
-        </React.Fragment>
-            
-            );
-    }
+        {/* <TopicChat socket={socket} topicid={topicid} /> */}
+      </React.Fragment>
+    );
+  }
 }
 
 export default Topics;
